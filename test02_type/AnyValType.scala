0ad7309389cba7e2ee8b21d3f07b52bf7e7a95ec
juneyoung <juneyoung85@gmail.com>
2014-02-06 17:29:27 +0900

    Any -> AnyVal -> Types

---
+++ test02_type/AnyValType.scala
@@ -0,0 +1,27 @@
+
+
+object main extends App {
+
+	//var is mutable, otherwise val is immutable
+	var db : Double = 3.14
+	var ft : Float = 3.14159265f
+	var bool : Boolean = true
+ 	var it : Int = 3
+	var lg : Long = 314159265l
+	var ch : Char = 'A' 
+
+	//List can contain may types at once
+	val list = List(db, ft, bool, it, lg, ch)
+	list.foreach(println)
+
+	//upcasting to abstract parent Type
+	for(i <- 0 until list.size){
+		//Scala 'Any' type is similar to java.lang.Object
+		//'AnyVal' extends 'Any', but since it is also abstract can not be initialized by new method
+		var any : Any = list(i)
+		println(any)
+	}
+
+	//Any type similar with java.lang.Object
+	
+}
